33f492b074ad27468ab7306721dd71cefbf9e6ac
ltronneb <ltronneberg@gmail.com>
2023-11-15 21:50:40 +0000

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='synpred',
+    version='0.0.1',
+    packages=['synpred'],
+    url='',
+    license='',
+    author='leiv',
+    author_email='',
+    description=''
+)
