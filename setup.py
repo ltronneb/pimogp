908aa6ef7245f8bade93be1a3c8b47ec8bb146c4
ltronneb <ltronneberg@gmail.com>
2023-11-16 12:56:09 +0000

    added initial notebook for PIICM implementation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='synpred',
     version='0.0.1',
-    packages=find_packages(),
+    packages=find_packages("."),
     url='',
     license='',
     author='leiv',
