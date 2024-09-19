5e351adb9b0e1791459c267e39f2e80c520edd7a
ltronneb <ltronneberg@gmail.com>
2024-09-19 13:14:13 +0100

    some minor fixes

--- pimogp/models/models.py
+++ pimogp/models/models.py
@@ -269,8 +269,6 @@ class DrugComboLMC_MKL(gpytorch.models.ApproximateGP):
         variational_strategy = SumVariationalStrategy(models)
 
 
-
-
         super(DrugComboLMC_MKL, self).__init__(variational_strategy)
 
         self.G = G
