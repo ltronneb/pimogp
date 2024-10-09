18abb11c33d708d1f86c4ce29f4bc3f5e7d8f2bf
ltronneb <ltronneberg@gmail.com>
2024-10-09 11:22:34 +0100

    minor change to utils.py in case of fixednoise likelihood

--- pimogp/utils/utils.py
+++ pimogp/utils/utils.py
@@ -1,10 +1,12 @@
 from copy import deepcopy
 import gc
+
+from gpytorch.likelihoods import FixedNoiseGaussianLikelihood
 from gpytorch.mlls import VariationalELBO
 import torch
 
 
-def better_varelbo_init(x, y, task_indices, model, likelihood, num_inits=100):
+def better_varelbo_init(x, y, noise, task_indices, model, likelihood, num_inits=100):
     with torch.no_grad():
         model.reinit()
         newmodel = deepcopy(model)
@@ -13,13 +15,19 @@ def better_varelbo_init(x, y, task_indices, model, likelihood, num_inits=100):
         likelihood.train()
         mll = VariationalELBO(likelihood, model, num_data=y.size(0))
         output = model(x, task_indices=task_indices)
-        loss = -mll(output, y)
+        if isinstance(likelihood, FixedNoiseGaussianLikelihood):
+            loss = -mll(output, y,noise=noise)
+        else:
+            loss = -mll(output, y)
         for i in range(num_inits):
             newmodel.reinit()
             newmodel.train()
             newmll = VariationalELBO(likelihood, newmodel, num_data=y.size(0))
             newoutput = newmodel(x, task_indices=task_indices)
-            newloss = -newmll(newoutput, y)
+            if isinstance(likelihood, FixedNoiseGaussianLikelihood):
+                newloss = -newmll(newoutput, y,noise=noise)
+            else:
+                newloss = -newmll(newoutput, y)
             # If this loss is better overwrite
             if newloss.item() < loss.item():
                 print("Old loss: ", loss.item())
@@ -27,6 +35,6 @@ def better_varelbo_init(x, y, task_indices, model, likelihood, num_inits=100):
                 model = deepcopy(newmodel)
                 loss = deepcopy(newloss)
 
-            # Delete everthing
+            # Delete everything
             gc.collect()
     return model, likelihood
