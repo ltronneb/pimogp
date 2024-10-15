a5497d6b794b858d6792964db687710cd6d45fd7
ltronneb <ltronneberg@gmail.com>
2024-10-15 13:15:51 +0200

    minor change to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+# Read the requirements from the requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='pimogp',
     version='0.0.1',
     packages=find_packages("."),
+    install_requires=requirements,
     url='',
     license='',
     author='leiv',
