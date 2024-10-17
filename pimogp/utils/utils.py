feb05a21b6ba67fd756efef9ca1c8f85b071e377
ltronneb <ltronneberg@gmail.com>
2024-10-17 21:24:16 +0200

    some more device fixes

--- pimogp/utils/utils.py
+++ pimogp/utils/utils.py
@@ -8,11 +8,17 @@ import torch
 
 def better_varelbo_init(x, y, noise, task_indices, model, likelihood, num_inits=100):
     with torch.no_grad():
+        device = next(model.parameters()).device # Need to make sure everything stays on the same device
+        #print("before reinit")
+        #print(next(model.parameters()).device)
         model.reinit()
+        model.to(device)
         newmodel = deepcopy(model)
         init_loss = []
         model.train()
         likelihood.train()
+        #print("after reinit")
+        #print(next(model.parameters()).device)
         mll = VariationalELBO(likelihood, model, num_data=y.size(0))
         output = model(x, task_indices=task_indices)
         if isinstance(likelihood, FixedNoiseGaussianLikelihood):
@@ -21,6 +27,7 @@ def better_varelbo_init(x, y, noise, task_indices, model, likelihood, num_inits=
             loss = -mll(output, y)
         for i in range(num_inits):
             newmodel.reinit()
+            newmodel.to(device)
             newmodel.train()
             newmll = VariationalELBO(likelihood, newmodel, num_data=y.size(0))
             newoutput = newmodel(x, task_indices=task_indices)
